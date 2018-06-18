6628c1781e75a79a864c7380379e55bb9adf938f
Kaitlyn McGoldrick <mcgoldrick.kaitlyn@gmail.com>
2018-06-18 13:57:07 -0400

    Clean up producer class

--- model/producer.py
+++ model/producer.py
@@ -10,27 +10,8 @@ class EnergyProducer(object):
             "battery": 3000
         }
 
-        # self.unit_price = resource_price[type]
-        # self.init_price = resource_init_price[type]
-
-    # OLD output function
-    # def output(self, energy_required, weather):
-    #     if self.type == "solar":
-    #         energy_produced = self.capacity[0] * weather
-
-    #     elif self.type == "wind":
-    #         energy_produced = self.capacity[0] * weather
-
-    #     else:
-    #         # fossil fuels makes exactly the amount needed
-    #         energy_produced = energy_required
-
-    #     energy_produced = min(energy_required, energy_produced)
-
     # To implement months, output will need time as a parameter
-    #TODO: check if weather is getting passed like OLD output function
     def output(self, quantity, coverage=1):
-        # Only need an if for solar because it is the only month dependent right now
         if self.type == "solar":
             energy_produced = min(quantity, self.capacity[0]*coverage)
         elif self.type == "wind":
