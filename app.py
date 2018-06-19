595b4195bf3a390f398ceee1b26be89150028fab
sneha <sneha.desai@mail.mcgill.ca>
2018-06-19 13:39:05 -0400

    added plot on webpage

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from main_page import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template("index.html")
+
+
+@app.route("/results")
+def results():
+    # res = main_function()
+    # return jsonify({"text": res})
+    return render_template("results.html")
 
 if __name__ == "__main__":
     app.run()
