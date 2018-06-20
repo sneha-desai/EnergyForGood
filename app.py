2ac23aa654a6059283a5fbeadd6242dd443117a2
Kelly Finnerty <kellyfinnerty2@mail.mcgill.ca>
2018-06-20 10:20:05 -0400

    website updates

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+@app.route("/team")
+def team():
+    return render_template("team.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 
 # @app.route("/results", methods="P")
 # def results():
